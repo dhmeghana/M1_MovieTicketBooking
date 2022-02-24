87fba4d28ac8f476a882e8d7362ca3f7cd4c7627
dhmeghana <dhmeghana23@gmail.com>
2022-02-24 21:16:14 +0530

    commit

---
+++ 3_Implementation/src/movielist.c
@@ -0,0 +1,15 @@
+#include "function.h"
+int ListofMovies(void)
+
+{
+	int i;
+	system("cls");
+	printf("\t\t\tMovie Name?\n");
+	printf("\t\t\t----------------------------\n\n");
+	printf("\t\t\tpress 1 for Spider Man\n\n");
+	printf("\t\t\tpress 2 for Conjuring\n\n");
+	printf("\t\t\tpress 3 for noddy kids\n");
+	scanf("%d",&i);
+	return i;
+}
+    
